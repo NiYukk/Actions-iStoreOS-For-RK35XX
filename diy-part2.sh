a0e8466442e6f29990f5ca4ad631d735eaafc897
thxg123 <86899054+thxg123@users.noreply.github.com>
2024-04-07 10:51:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -69,3 +69,22 @@ sed -i "s/panther,x2|\\\/panther,x2|\\\\\n	dg,nas|\\\/g" target/linux/rockchip/r
 cp -f $GITHUB_WORKSPACE/file/rk3568-firefly-roc-pc-se-core.dtsi target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se-core.dtsi
 
 cp -f $GITHUB_WORKSPACE/file/rk3568-firefly-roc-pc-se.dts target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se.dts
+
+# adguardhome
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
+# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+
+# openclash
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
+# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
+# 加入OpenClash核心
+chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+if [ "$1" = "rk33xx" ]; then
+    $GITHUB_WORKSPACE/preset-clash-core.sh arm64
+elif [ "$1" = "rk35xx" ]; then
+    $GITHUB_WORKSPACE/preset-clash-core.sh arm64
+elif [ "$1" = "x86" ]; then
+    $GITHUB_WORKSPACE/preset-clash-core.sh amd64
+fi
